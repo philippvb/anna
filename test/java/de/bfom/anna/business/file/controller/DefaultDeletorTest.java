f88cc07b0917a3b922d469e3d7bc46a314c03585
philippvb <philippvonbachmann@web.de>
2019-05-06 11:03:52 +0100

    improved delete test

--- test/java/de/bfom/anna/business/file/controller/DefaultDeletorTest.java
+++ test/java/de/bfom/anna/business/file/controller/DefaultDeletorTest.java
@@ -1,21 +1,19 @@
 package de.bfom.anna.business.file.controller;
 
-import de.bfom.anna.business.file.controller.FileController;
-import de.bfom.anna.business.file.entity.FileEntity;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultDeletorTest {
-    private EntityManagerFactory myfactory =Persistence.createEntityManagerFactory("MeineJpaPU");
-    private FileController mycontroller = FileController.defaultinit(myfactory, null);
+    EntityManagerFactory myfactory = Persistence.createEntityManagerFactory("MeineJpaPU");
+    FileController mycontroller = FileController.defaultinit(myfactory, null);
 
 
     @BeforeEach
--- test/java/de/bfom/anna/business/file/controller/DefaultRetrieverTest.java
+++ test/java/de/bfom/anna/business/file/controller/DefaultRetrieverTest.java
@@ -1,55 +1,54 @@
 package de.bfom.anna.business.file.controller;
 
-import de.bfom.anna.business.file.controller.FileController;
 import de.bfom.anna.business.file.controller.Retrieve.DefaultRetriever;
+import de.bfom.anna.business.file.controller.Retrieve.Retrieve;
 import de.bfom.anna.business.file.entity.FileEntity;
 import de.bfom.anna.business.file.entity.ReducedFileEntity;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultRetrieverTest {
-    EntityManagerFactory myfactory;
-    FileController mycontroller;
+    EntityManagerFactory myfactory = Persistence.createEntityManagerFactory("MeineJpaPU");
+    FileController mycontroller = FileController.defaultinit(myfactory, null);
     FileEntity del;
-    DefaultRetriever ret;
-
+    Retrieve ret = new DefaultRetriever(myfactory);
 
     @BeforeEach
+    @AfterEach
     void init(){
-        myfactory = Persistence.createEntityManagerFactory("MeineJpaPU");
-        mycontroller = FileController.defaultinit(myfactory, null);
-        ret = new DefaultRetriever(myfactory);
+        assertEquals(0, mycontroller.retrieveAll().size());
     }
 
     @Test
     void retrieve() {
-        assertEquals(null, mycontroller.retrieve(1));
         mycontroller.persist(new File("src/testfiles/test.txt"));
         assertTrue(mycontroller.retrieve(1) != null);
         mycontroller.delete(1);
-        assertEquals(null, mycontroller.retrieve(1));
     }
 
     @Test
     void retrieveByName(){
-        mycontroller.persist(new File("src/testfiles/test.txt"));
         mycontroller.persist(new File("src/testfiles/test.txt"));
         assertFalse(ret.retrieve("test").isEmpty());
     }
 
     @Test
     void retrieveAllReduced(){
-        assertEquals(0, mycontroller.retrieveAll().size());
         mycontroller.persist(new File("src/testfiles/test.txt"));
+        mycontroller.persist(new File("src/testfiles/testpicture.jpg"));
         assertEquals(ReducedFileEntity.class, mycontroller.retrieveAllReduced().get(0).getClass());
-        mycontroller.delete(1);
+        assertFalse(mycontroller.retrieveAllReduced().isEmpty());
+        mycontroller.delete(mycontroller.retrieveAllReduced().get(0).getID());
+        mycontroller.delete(mycontroller.retrieveAllReduced().get(0).getID());
 
     }
 }
